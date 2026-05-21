b5a86f685bfccd7651cf2d04b3ee3543962fb966
Hiwot Abere <hiwotabere310@gmail.com>
2026-05-21 10:41:51 +0300

    created average function

--- src/calculator.js
+++ src/calculator.js
@@ -16,10 +16,18 @@ function divide(a, b) {
   }
   return a / b;
 }
+function average(numbers){
+  if (numbers.length === 0) {
+    throw new Error("Cannot calculate the average of an empty array");
+  } 
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  return sum / numbers.length;
+}
 
 module.exports = {
   add,
   subtract,
   multiply,
-  divide
+  divide,
+  average
 };
